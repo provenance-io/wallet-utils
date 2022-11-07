d60c6d65614ee4f2bff0c4c3ef38d30308a603c0
Jarryd Allison <jallison@figure.com>
2022-11-07 12:30:18 -0700

    fix(supportdenoms type removal): removed SupportedDenoms type
    Removed SupportedDenoms type from the repo
    
    #23

--- src/utils/display/index.ts
+++ src/utils/display/index.ts
@@ -4,5 +4,4 @@ export * from './displayMetadataFields';
 export * from './getJSType';
 export * from './hashFormat';
 export * from './numberFormat';
-export * from './readableDenom';
 export * from './trimString';
