d8276c7401e3c55dcc350e91e5121831ac1cd490
Jarryd Allison <jallison@figure.com>
2022-11-14 15:43:35 -0700

    Final updates to this branch

--- src/services/message-service.ts
+++ src/services/message-service.ts
@@ -57,7 +57,7 @@ import {
 import { MsgAddMarkerRequest } from '../proto/provenance/marker/v1/tx_pb';
 import { MarkerStatus, MarkerType } from '../proto/provenance/marker/v1/marker_pb';
 import { Access } from '../proto/provenance/marker/v1/accessgrant_pb';
-import { formatCustomObj, formatSingleValue, getJSType } from '../utils';
+import { formatCustomObj, formatSingleValue } from '../utils';
 import { isMatching, P } from 'ts-pattern';
 
 export type GenericDisplay = { [key: string]: any };
